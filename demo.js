4ee0fbaf5ba17f768ae15c5322f3e1f47c39c122
Kannan.N <kannangk2277@gmail.com>
2025-11-11 19:43:29 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,11 @@
+function fun1()
+{
+    x=document.getElementById("navbar")
+    if(x.style.display = "none")
+    {
+        x.style.display = "block"
+    }
+    else{
+        x.style.display = "none"
+    }
+}
